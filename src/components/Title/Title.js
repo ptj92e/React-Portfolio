3d7204c52b944c9d91228463205ee34210554674
Phillip Jones <ptj92e@gmail.com>
2020-03-22 21:09:19 -0500

    Added landing page

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+    return (
+        <div className="container">
+            <h4>Welcome to the portfolio of</h4>
+            <h1>Phillip Jones</h1>
+            <h4>Full Stack Web Developer</h4>
+        </div>
+    );
+};
+
+export default Title;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Title from "../components/Title/Title";
+
+function Home() {
+    return(
+        <div>
+            <Navbar />
+            <Title />
+        </div>
+    );
+};
+
+export default Home;
