813f478ea291be8c56c8a03b411386d8645db45f
Phillip Jones <ptj92e@gmail.com>
2020-03-23 16:37:50 -0500

    Added Styling to project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,15 +5,17 @@ import { prototype } from "events";
 function ProjectCard(props) {
     return (
         <div className={props.className} id={props.id}>
-            <img src={props.image} alt={props.title}/>
-            <h1>{props.title}</h1>
-            <h3>Technologies:</h3>
-            <p>{props.technologies}</p>
-            <h3>Description:</h3>
-            <p>{props.description}</p>
-            <h3>Links:</h3>
-            <p>GitHub: <a href={props.github}>{props.title}</a></p>
-            <p>GitHub: <a href={props.heroku}>{props.title}</a></p>
+            <img src={props.image} alt={props.title} />
+            <div>
+                <h1>{props.title}</h1>
+                <h3>Technologies:</h3>
+                <p>{props.technologies}</p>
+                <h3>Description:</h3>
+                <p>{props.description}</p>
+                <h3>Links:</h3>
+                <p>GitHub: <a href={props.github}>{props.title}</a></p>
+                <p>GitHub: <a href={props.heroku}>{props.title}</a></p>
+            </div>
         </div>
     )
 }
