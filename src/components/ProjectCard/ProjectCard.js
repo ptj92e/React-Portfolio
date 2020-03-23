30bcccdccdbcb221c6548327448bb522ff87e33a
Phillip Jones <ptj92e@gmail.com>
2020-03-23 14:11:34 -0500

    Mapping over json

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ProjectCard.css";
+import { prototype } from "events";
+
+function ProjectCard(props) {
+    return (
+        <div>
+            <img src={props.image} alt={props.title}/>
+            <h1>{props.title}</h1>
+            <h3>Technologies:</h3>
+            <p>{props.technologies}</p>
+            <h3>Description:</h3>
+            <p>{props.description}</p>
+            <h3>Links:</h3>
+            <p>GitHub: <a href={props.github}>{props.title}</a></p>
+            <p>GitHub: <a href={props.heroku}>{props.title}</a></p>
+        </div>
+    )
+}
+
+export default ProjectCard;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ProjectTitle from "../ProjectTitle/ProjectTitle";
+import ProjectCard from "../ProjectCard/ProjectCard";
+import data from "../../utils/data.json";
+// import Wrapper from "../Wrapper/Wrapper";
 
-function Projects() {
-    return (
-        <div>
-            <ProjectTitle />
-        </div>
-    );
+class Projects extends Component {
+    state = {
+        projects: data.projects
+    }
+    render() {
+        return (
+            <div>
+                <ProjectTitle />
+                <div className="container projectContainer">
+                    {this.state.projects.map(project => (
+                        <ProjectCard
+                            title={project.title}
+                            technologies={project.technologies}
+                            description={project.description}
+                            image={project.image}
+                            github={project.github}
+                            heroku={project.heroku}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Projects;
