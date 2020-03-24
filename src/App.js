fd411c58b206fd1d32d09840ff064b3b15a8cb02
Phillip Jones <ptj92e@gmail.com>
2020-03-24 18:38:08 -0500

    Added color scheme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Portfolio from "./components/Portfolio/Portfolio";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky">
+        <nav className="navbar navbar-expand-lg sticky">
             <a className="navbar-brand" href="#">Phillip Jones</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
