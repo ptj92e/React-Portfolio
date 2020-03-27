e20274cac3f6256a30a97ab308e4ccacfc916dda
Phillip Jones <ptj92e@gmail.com>
2020-03-27 08:48:21 -0500

    Finished styling contact page

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -4,12 +4,13 @@ import "./SocialMedia.css";
 function SocialMedia() {
     return (
         <div id="socialDiv">
-            <h1>Social Media</h1>
+            <h1>Let's Get In Touch!</h1>
             <div id="iconsDiv">
                 <a href="https://www.facebook.com/TrombonePhil92" target="_blank" className="fab fa-facebook fa-2x"></a>
                 <a href="https://twitter.com/TrombonePhil92" target="_blank" className="fab fa-twitter fa-2x"></a>
                 <a href="https://www.linkedin.com/in/p-jones92" target="_blank" className="fab fa-linkedin fa-2x"></a>
                 <a href="https://github.com/ptj92e" target="_blank" className="fab fa-github fa-2x"></a>
+                <a href="https://drive.google.com/open?id=1w986VtOVbPJq7OfwunDdB7Phe3j6Xeg22XjMgksMvZw" target="_blank" className="fas fa-file-word fa-2x"></a>
             </div>
         </div>
     )
