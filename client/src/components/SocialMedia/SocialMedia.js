7a0830d1a27682930a27a5d29f445864b0a76bff
Phillip Jones <ptj92e@gmail.com>
2020-03-26 20:47:44 -0500

    Fixed social media links

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -6,10 +6,10 @@ function SocialMedia() {
         <div id="socialDiv">
             <h1>Social Media</h1>
             <div id="iconsDiv">
-                <a href="#" className="fab fa-facebook fa-2x"></a>
-                <a href="#" className="fab fa-twitter fa-2x"></a>
-                <a href="#" className="fab fa-linkedin fa-2x"></a>
-                <a href="#" className="fab fa-github fa-2x"></a>
+                <a href="https://www.facebook.com/TrombonePhil92" target="_blank" className="fab fa-facebook fa-2x"></a>
+                <a href="https://twitter.com/TrombonePhil92" target="_blank" className="fab fa-twitter fa-2x"></a>
+                <a href="https://www.linkedin.com/in/p-jones92" target="_blank" className="fab fa-linkedin fa-2x"></a>
+                <a href="https://github.com/ptj92e" target="_blank" className="fab fa-github fa-2x"></a>
             </div>
         </div>
     )
