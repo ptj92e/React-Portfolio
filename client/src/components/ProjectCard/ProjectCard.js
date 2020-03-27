cc3e6443df82f21a108712e10be6d2c218c0f036
Phillip Jones <ptj92e@gmail.com>
2020-03-27 10:22:15 -0500

    Fixed project cards

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -4,12 +4,12 @@ import "./ProjectCard.css";
 function ProjectCard(props) {
     return (
         <div className={props.className} id={props.id}>
+            <img src={props.image} alt={props.title} />
             <h1>{props.title}</h1>
             <div id="linkDiv">
                 <a href={props.github}>GitHub</a>
                 <a href={props.heroku}>Heroku</a>
             </div>
-            <img src={props.image} alt={props.title} />
             <div id="projectText">
                 <p><b>Technologies:</b> {props.technologies}</p>
                 <h3>Description:</h3>
