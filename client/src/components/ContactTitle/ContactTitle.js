02896c4cefe2bbffa2e5231acaba53b74114cd7d
Phillip Jones <ptj92e@gmail.com>
2020-03-26 20:21:30 -0500

    Updated color scheme

--- client/src/components/ContactTitle/ContactTitle.js
+++ client/src/components/ContactTitle/ContactTitle.js
@@ -4,7 +4,8 @@ import "./ContactTitle.css";
 function ContactTitle() {
     return (
         <div className="contactTitle">
-            <h1>Contact Phillip</h1>
+            <h1>Contact Me:</h1>
+            <p>Spicy jalapeno bacon ipsum dolor amet pariatur hamburger deserunt, chislic minim ut ullamco jowl. Bacon meatloaf pork belly tempor frankfurter anim bresaola in excepteur filet mignon shankle chicken incididunt. Ham hock incididunt dolor, pork belly labore enim qui culpa mollit salami. Qui swine aute burgdoggen, minim tongue commodo aliquip shoulder rump short ribs pig corned beef meatloaf ipsum.</p>
         </div>
     );
 };
--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import ContactTitle from "../ContactTitle/ContactTitle";
 import SocialMedia from "../SocialMedia/SocialMedia";
 import ContactForm from "../ContactForm/ContactForm";
 
-function Contact() {
+function Contact(props) {
     return (
         <div>
             <ContactTitle />
