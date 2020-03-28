48471f57e596264f90c208b96f8d63dd25c9cdfc
Phillip Jones <ptj92e@gmail.com>
2020-03-27 20:58:02 -0500

    Fixed canvas

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -3,20 +3,41 @@ import "./Canvas.css";
 
 class Canvas extends Component {
     componentDidMount() {
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
-        window.onload = () => {
-            console.log("Canvas Loaded");
-            ctx.beginPath();
-            ctx.rect(0, 0, 1500, 1000);
-            ctx.fillStyle = "#25274d";
-            ctx.fill();
+        let c = document.getElementById("canvas");
+        let ctx = c.getContext("2d");
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+        let binary = "01";
+        binary = binary.split("");
+        let fontSize = 20;
+        let columns = c.width/fontSize;
+        let drops = [];
+        for (let x = 0; x < columns; x++)
+        drops[x] = 1;
+        function draw() {
+            ctx.fillStyle = "rgba(37, 39, 77, 0.2)";
+            ctx.fillRect(0, 0, c.width, c.height);
+            ctx.fillStyle = "#aaabb8";
+            ctx.font = fontSize + "px Arial";
+            for (let i = 0; i < drops.length; i++) {
+                let text = 0;
+                if (i % 2 === 0) {
+                    text = binary[0];
+                } else {
+                    text = binary[1]
+                }
+                ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+                if (drops[i] * fontSize > c.height && Math.random() > 0.975)
+                drops[i] = 0;
+                drops[i]++;
+            }
         }
+        setInterval(draw, 100);
     }
     render() {
         return (
             <div>
-                <canvas id="canvas" width={1500} height={1500} />
+                <canvas id="canvas" />
             </div>
         )
     }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Title from "../Title/Title";
 import About from "../About/About";
-// import Canvas from "../Canvas/Canvas";
+import Canvas from "../Canvas/Canvas";
 
 function Home() {
     return(
         <div>
-            {/* <Canvas /> */}
+            <Canvas />
             <Title />
             <About />
         </div>
