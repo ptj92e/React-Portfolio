dbda5a813fe4595c9549ae094510ab5ea750e5c8
Phillip Jones <ptj92e@gmail.com>
2020-04-02 08:35:42 -0500

    Please work

--- routes/html/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const htmlRoutes = require("./apiRoutes");
-
-router.use("/projects", htmlRoutes);
-router.use("/contact", htmlRoutes);
-
-module.exports = router;
