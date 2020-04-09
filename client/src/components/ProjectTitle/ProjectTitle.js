872c0ff5c3cd5667123b3fe02ae91b0f31a58f65
unknown <ptj92e@mgmail.com>
2020-04-08 21:12:20 -0500

    Fixed typo in projects page

--- client/src/components/ProjectTitle/ProjectTitle.js
+++ client/src/components/ProjectTitle/ProjectTitle.js
@@ -5,7 +5,7 @@ function ProjectTitle() {
     return (
         <div className="projectTitle">
             <h1>My Work:</h1>
-            <p>I am a full stack web developer with a passion for responsive user experiences.<br />I enjoy working with HTML, CSS, and JavaScript, but my favorite front end framework is React.js. My projects also include mySQL and MongoDB databased featuring Node.js and Express.js servers.</p>
+            <p>I am a full stack web developer with a passion for responsive user experiences.<br />I enjoy working with HTML, CSS, and JavaScript, but my favorite front end framework is React.js. My projects also include mySQL and MongoDB databases featuring Node.js and Express.js servers.</p>
         </div>
     );
 };
