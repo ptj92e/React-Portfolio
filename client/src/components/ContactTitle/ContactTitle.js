465db1849c289781547bdf7b35c1f559a0772770
Phillip Jones <ptj92e@gmail.com>
2020-04-22 09:13:32 -0500

    Added Email Address

--- client/src/components/ContactTitle/ContactTitle.js
+++ client/src/components/ContactTitle/ContactTitle.js
@@ -5,7 +5,7 @@ function ContactTitle() {
     return (
         <div className="contactTitle">
             <h1>Contact Me:</h1>
-            <p>If you would like to contact me directly, please use the form located below and I will get back to you as soon as I can.<br />At the bottom of the page, you can find links to Facebook, Twitter, LinkedIn, GitHub, and my resume.<br />I am looking forward to working with you.</p>
+            <p>If you would like to contact me, please use the form located below and I will get back to you as soon as I can. <br/>Otherwise, my E-Mail address is: ptj92e@gmail.com<br />At the bottom of the page, you can find links to Facebook, Twitter, LinkedIn, GitHub, and my resume.<br />I am looking forward to working with you.</p>
         </div>
     );
 };
