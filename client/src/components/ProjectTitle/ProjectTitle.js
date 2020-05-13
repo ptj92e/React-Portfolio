177044cdf276ba45a871be9a22119c1f61d7663b
Phillip Jones <ptj92e@gmail.com>
2020-05-13 08:28:30 -0500

    Updated Projects

--- client/src/components/ProjectTitle/ProjectTitle.js
+++ client/src/components/ProjectTitle/ProjectTitle.js
@@ -5,7 +5,7 @@ function ProjectTitle() {
     return (
         <div className="projectTitle">
             <h1>My Work:</h1>
-            <p>I am a full stack web developer with a passion for responsive user experiences.<br />I enjoy working with HTML, CSS, and JavaScript, but my favorite front end framework is React.js. My projects also include mySQL and MongoDB databases featuring Node.js and Express.js servers.</p>
+            <p>I am a full stack web developer with a passion for responsive user experiences.<br />I enjoy working with HTML, CSS, and JavaScript, but my favorite front end framework is React.js. I am currently building my knowledge of Object Oriented languages like Python through building servers for my React projects. My projects also include mySQL and MongoDB databases featuring Node.js and Express.js servers.</p>
         </div>
     );
 };
